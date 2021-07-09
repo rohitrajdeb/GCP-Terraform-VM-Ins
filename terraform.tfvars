f0ab191e2d8fbfaa5a628c1af2629f491d1a6d3b
Rohit Rajdeb <rohit.rajdeb@gmail.com>
2021-07-09 23:16:51 +0100

    TF Project to create GCP Compute Engine

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+project = "just-function-319021"
+region = "us-central1"
+zone = "us-central1-a"
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+    type = string
+    description = "The Project Id"
+}
+variable "region" {
+    type = string
+    description = "The Region where the VM Instance is to be created"
+}
+variable "zone" {
+    type = string
+    description = "The zone within the Region"
+}
